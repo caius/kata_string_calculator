d586b949d49b334e11b57dd4f965f1e4d849fec8
Caius Durling <dev@caius.name>
2014-01-23 22:27:10 +0000

    Implement multiple delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,16 +1,20 @@
 def Add(input)
-  numbers = input.match(%r{\A(?:\/\/(.|\[.+\])\n)?(.+)\z}m) do |match|
-    _, delim, str = match.to_a
-    delim = delim[1..-2] if delim && delim[/\[.+\]/]
-    delim ||= /(?:,|\n)/
-    str.split(delim).map(&:to_i)
+  delimiters = []
+
+  if input["//"]
+    # We have delimiters present!
+    delims, input = input.split("\n")
+    delimiters.push *(delims["["] ? delims.scan(/\[(.+?)\]/).flatten : delims[3..-1])
+  else
+    delimiters << "," << "\n"
   end
 
-  return 0 unless numbers
+  splitter = delimiters.map {|x| Regexp.escape(x) }.join("|")
+  numbers = input.split(Regexp.new(splitter)).map(&:to_i)
 
   if (( negatives = numbers.reject {|i| i.abs == i } )).any?
     raise "negative numbers not allowed: #{negatives.join(", ")}"
   end
 
-  numbers.reject {|i| i >= 1000 }.inject(:+)
+  numbers.reject {|i| i >= 1000 }.inject(0, :+)
 end
